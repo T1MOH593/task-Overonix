d382b12e8414b533180f40db6e33c279730ebaf6
Uladzislau Andryieuski <vsadovnikov78@gmail.com>
2021-07-28 11:04:14 +0300

    Enable security

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,23 @@ CREATE TABLE coordinates (
     lon VARCHAR (30) ,
     PRIMARY KEY (lat, lon)
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR (15) PRIMARY KEY ,
+    password VARCHAR (100),
+    enabled SMALLINT
+);
+
+CREATE TABLE IF NOT EXISTS authorities (
+    username varchar(15),
+    authority varchar(25),
+    FOREIGN KEY (username) references users(username)
+);
+
+INSERT INTO users (username, password, enabled)
+VALUES
+('user', '{noop}user', 1);
+
+INSERT INTO authorities (username, authority)
+VALUES
+('user', 'ROLE_USER');
